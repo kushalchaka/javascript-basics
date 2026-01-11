fee9fa8c6eccbf37994487f27dc1d4c2a0bda1d7
kushalchaka <kushalchaka@gmail.com>
2026-01-10 18:23:07 -0600

    basics

---
+++ website/index.js
@@ -0,0 +1,8 @@
+//console.log(`Hello`);
+//console.log(`I like pizza`);
+
+//window.alert(`This is an alert`);
+//window.alert(`I really like pizza`);
+
+document.getElementById("myH1").textContent = `Hello`;
+document.getElementById("myP").textContent = `I really like pizza!`;
